fd03550ed2c836143df70361e6131f6e6a747d02
lojason71 <lojason71@gmail.com>
2023-07-11 21:42:34 -0700

    Implemented Chessboard Piece

--- src/Components/Chessboard.js
+++ src/Components/Chessboard.js
@@ -1,4 +1,5 @@
 import './Chessboard.css';
+import ChessboardPiece from './ChessboardPiece';
 
 export default function Chessboard() {
 
@@ -7,22 +8,30 @@ export default function Chessboard() {
         for(let i = 0; i < 8; i++) {
             for(let j = 0; j < 8; j++) {
                 const cellColor = (i + j) % 2 === 0 ? 'dodgerblue' : '#363457';
+                const piece = "pawn";
                 chessboard.push(
                     <div
                         key={`${i}${j}`}
                         className="chessboard_cell"
                         style={{backgroundColor: cellColor}}
-                    />
+                    >
+                        <ChessboardPiece
+                            key={`${i}-${j}`}
+                            className="chessboard_piece"
+                            style={{backgroundColor: cellColor}}
+                            piece={piece}
+                        />
+                    </div>
                 )
             }
         }
         return chessboard;
     }
 
+
     return (
         <div>
             <div className="chessboard">
-                {/* <div> HELLO WORLD PENIS </div> */}
                 {chessboardRender()}
             </div>
         </div>
