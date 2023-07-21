2dfb0898f01463def1887168b752387561978d36
fulong <fulonghuang05@gmail.com>
2023-07-20 20:12:53 -0700

    feat: server open rooms (port) per client request

--- client/client.js
+++ client/client.js
@@ -1,23 +1,59 @@
-const socket = new WebSocket("ws://localhost:8080");
+const body = document.getElementById('body');
 
-//socket.addEventListener("open", (event) => {
-//    socket.send("HELLO SERVER!");
-//});
+getAvailableRooms();
 
-socket.addEventListener("message", (event) => {
-    console.log("message from server: ", event.data);
-    chatContent.append(event.data);
-    let p = document.createElement('br');
-    chatContent.append(p);
-});
+async function getAvailableRooms(){
+    const response = await fetch('http://localhost:3000');
+    const data = await response.json();
+    for(const room of data){
+        let b = document.createElement('button');
+        b.textContent = room;
+        b.onclick = connectToRoom.bind(null, room);
+        body.append(b);
+    }
+}
 
-const inputBox = document.getElementById('input-box-client');
-const chatContent = document.getElementById('chat-content');
+async function createRoom(){
+    const response = await fetch('http://localhost:3000/create');
+    const data = await response.json();
+    connectToRoom(data);
+}
 
-function onClickSend(){
-    socket.send(inputBox.value);
-    console.log(inputBox.value)
-    inputBox.value = "";
+function connectToRoom(portNum){
+    body.textContent = 'Text: ';
+    const inputBox = document.createElement('input');
+    const button = document.createElement('button');
+    button.addEventListener('click', onClickSend);
+    button.textContent = 'Send';
+    const chatTitle = document.createElement('h2');
+    chatTitle.textContent = 'Chat:';
+    const chatContent = document.createElement('p');
+    body.append(inputBox);
+    body.append(button);
+    body.append(chatTitle);
+    body.append(chatContent);
+//    const inputBox = document.getElementById('input-box-client');
+//    const chatContent = document.getElementById('chat-content');
+
+    const socket = new WebSocket(`ws://localhost:${portNum}`);
+    console.log(`connected to port ${portNum}`);
+
+    //socket.addEventListener("open", (event) => {
+    //    socket.send("HELLO SERVER!");
+    //});
+
+    socket.addEventListener("message", (event) => {
+        console.log("message from server: ", event.data);
+        chatContent.append(event.data);
+        let p = document.createElement('br');
+        chatContent.append(p);
+    });
+
+    function onClickSend(){
+        socket.send(inputBox.value);
+        console.log(inputBox.value)
+        inputBox.value = "";
+    }
 }
 
 
--- server/server.js
+++ server/server.js
@@ -1,42 +1,97 @@
+
+// API:
+const express = require('express');
+const app = express();
+const rooms = [];
+
+
+// Cors Setting
+const cors = require('cors');
+
+const corsOptions = {
+    origin: '*',
+}
+
+app.use(cors(corsOptions));
+
+
+app.get('/create', async (req, res) => {
+    let portNum = await findAvaliablePort();
+    if(portNum === 0){
+        res.end('0');
+        console.log("PORT IS 0");
+        return;
+    }
+    openNewSocket(portNum);
+    res.end(JSON.stringify(portNum))
+    rooms.push(portNum);
+});
+
+app.get('/', async(req, res) => {
+    res.end(JSON.stringify(rooms));
+    console.log("SLDIFJOWIPEG");
+});
+
+
+app.listen(3000, () =>{
+    console.log("server listening at port: ", 3000);
+});
+
+
+
+// Web Socket
 const ws = new require('ws');
-const wss = new ws.Server({port: 8080});
+const portscanner = require('portscanner');
 
-const clients = new Set();
+const startPort = 2000;
+const endPort = 20000;
+async function findAvaliablePort(){
+    return await portscanner.findAPortNotInUse(startPort, endPort, '127.0.0.1');
+}
+
+function openNewSocket(port){
+    const wss = new ws.Server({port: port});
+
+    const clients = new Set();
 
 
-wss.on('connection', (ws) => {
-    clients.add(ws);
-    console.log(`New Connection, currently ${clients.size} online`);
-    ws.send("Welcome");
+    wss.on('connection', (ws) => {
+        clients.add(ws);
+        console.log(`Port: ${port}, New Connection, currently ${clients.size} online`);
+        ws.send("Welcome");
 
-    let clientName;
+        let clientName;
 
-    ws.on('message', function(message_BINARY) {
-        console.log("What", message_BINARY);
-        let message = String.fromCharCode(...message_BINARY);
-        console.log("Message is: ", message);
-        if(clientName){
-            message = `${clientName} : ${message}`;
+        ws.on('message', function(message_BINARY) {
+            // binary to string
+            let message = String.fromCharCode(...message_BINARY);
+            if(clientName){
+                message = `${clientName} : ${message}`;
+                for(let client of clients){
+                    client.send(message);
+                }
+                return;
+            }
+            clientName = message;
+            console.log("Set new client name to: ", clientName);
+            message = `${clientName} JOINED`;
             for(let client of clients){
                 client.send(message);
             }
-            return;
-        }
-        clientName = message;
-        console.log("Set new client name to: ", clientName);
-        message = `${clientName} JOINED`;
-        for(let client of clients){
-            client.send(message);
-        }
-    });
+        });
 
-    ws.on('close', () =>{
-        clients.delete(ws);
-        if(!clientName) return;
-        for(let client of clients){
-            client.send(`${clientName} DISCONNECTED`);
-        }
-        console.log("Connection closed");
+        ws.on('close', () =>{
+            clients.delete(ws);
+            if(!clientName) return;
+            for(let client of clients){
+                client.send(`${clientName} DISCONNECTED`);
+            }
+            if(clients.size === 0){
+                ws.close();
+            }
+            console.log("Connection closed");
+        });
     });
-});
+    console.log("CREATED PORT: ", port);
+}
 
